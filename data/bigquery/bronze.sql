b5919479b10cd8595911c293ec1fd3855a356999
ajaydandge60 <ajay.dandge60@gmail.com>
2026-02-21 15:31:13 +0530

    done auto 3

--- data/bigquery/bronze.sql
+++ data/bigquery/bronze.sql
@@ -1,34 +1,29 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS `evident-factor-479408-a9.bronze_dataset.departments` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare_b1
-/landing/hospital/departments/*.json']
+  uris = ['gs://healthcare_b1/landing/hospital/departments/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `evident-factor-479408-a9.bronze_dataset.encounters` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare_b1
-/landing/hospital/encounters/*.json']
+  uris = ['gs://healthcare_b1/landing/hospital/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `evident-factor-479408-a9.bronze_dataset.patients` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare_b1
-/landing/hospital/patients/*.json']
+  uris = ['gs://healthcare_b1/landing/hospital/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `evident-factor-479408-a9.bronze_dataset.providers` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare_b1
-/landing/hospital/providers/*.json']
+  uris = ['gs://healthcare_b1/landing/hospital/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `evident-factor-479408-a9.bronze_dataset.transactions` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare_b1
-/landing/hospital/transactions/*.json']
+  uris = ['gs://healthcare_b1/landing/hospital/transactions/*.json']
 );
